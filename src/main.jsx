d20570826105492bde13c9bb0f36075a5cccddda
Burman Bedford <burmanbed@gmail.com>
2023-08-01 21:53:47 -0400

    using the first card as a template. Created the remaining cards with the map method

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Card from './assets/Card'
+import CardList from './assets/CardList'
 import './index.css'
 import "tachyons"
 import { robots } from './assets/robots'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Card id = {robots[1].id} name = {robots[1].name} email = {robots[1].email}/>
+    <CardList robots = {robots} />
   </React.StrictMode>,
 )
